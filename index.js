87360d69e4d387675a8ab67ec05bbbcfede37eb3
wickyvinn <vicky.winn@gmail.com>
2015-08-31 00:14:25 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('main.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
